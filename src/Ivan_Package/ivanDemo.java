aa654521159dcecd8fdb3ac093d9f6b4520d2f3f
pepep <pepep@Matte>
2023-04-11 15:22:19 -0400

    initial commit

---
+++ src/Ivan_Package/ivanDemo.java
@@ -0,0 +1,15 @@
+package Ivan_Package;
+
+public class ivanDemo {
+
+	public static void main (String [] args) {
+		System.out.println("Hello Java");
+	}
+	
+	public static void getName () {
+		String firstName = "Ivan";
+	System.out.println(firstName);
+}
+}
+
+
