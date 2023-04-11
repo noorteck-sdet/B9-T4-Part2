5df62df2a2b921282082978b5249fbaabacbcea0
denni <denni@DennissePC>
2023-04-10 23:30:54 -0400

    Dennisse

---
+++ src/dennisse/HWClass1.java
@@ -0,0 +1,18 @@
+package dennisse;
+
+public class HWClass1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("testing");
+		
+	}
+	
+	public void getResult() {
+		
+		String name = "Dennisse";
+		
+		System.out.println(name);
+	}
+
+}
