bbc97c567ea4654767777ad4d30d101933bd6d62
GE66_Rider <GE66_Rider@DESKTOP-M5L0776>
2023-04-10 23:09:36 -0400

    Luis espinal HW

---
+++ src/LuisEspinal_Package/LuisEspinalDemo.java
@@ -0,0 +1,13 @@
+package LuisEspinal_Package;
+
+public class LuisEspinalDemo {
+	
+	public void Test1() {
+		System.out.println("Test one");
+	}
+	
+	void Test2() {
+		System.out.println("Test two");
+	}
+
+}
