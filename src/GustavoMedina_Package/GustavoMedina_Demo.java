3bc794618864eee247f2e9a00a50a669c91e02bb
Gusta <Gusta@DESKTOP-MSFL281>
2023-04-10 21:19:58 -0600

    Initial Commit

---
+++ src/GustavoMedina_Package/GustavoMedina_Demo.java
@@ -0,0 +1,18 @@
+package GustavoMedina_Package;
+
+public class GustavoMedina_Demo {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello");
+		
+		
+		
+	}
+	
+	
+	
+
+}
