94f6da6cd939c2c65692abe29ebf8a218d0483ab
Edy Rodriguez <Edy Rodriguez@LAPTOP-QFB1AMLV>
2023-04-12 16:28:36 -0600

    Initial commit

---
+++ src/Edy_Package/EdyRodriguezDemo.java
@@ -0,0 +1,16 @@
+package Edy_Package;
+
+public class EdyRodriguezDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void getResult() {
+					
+	}
+	
+	public void hwTest() {
+		
+	}
+}
