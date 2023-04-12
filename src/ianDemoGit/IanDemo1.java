f6ea4e5c9edc23571a7c94243dd4b2c30945126b
Gusta <Gusta@DESKTOP-MSFL281>
2023-04-12 15:47:51 -0600

    Update From Gustavo Medina

--- src/ianDemoGit/IanDemo1.java
+++ src/ianDemoGit/IanDemo1.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 	System.out.println("Firat Comment");
 	
 	
-	System.out.println("Hello Ian From Gustavo Medina");
+	System.out.println("Hello Ian From Gustavo Medina"); 
 }
 
 
