249457497b5cb7afd86fc1480895a40587de8b44
Gusta <Gusta@DESKTOP-MSFL281>
2023-04-12 15:58:49 -0600

    Merge branch 'master' of https://github.com/noorteck-sdet/B9-T4-Part2.git

--- src/ianDemoGit/IanDemo1.java
+++ src/ianDemoGit/IanDemo1.java
@@ -4,6 +4,11 @@ public class IanDemo1 {
 	
 public static void main(String[] args) {
 	
+	String group = "B9 team4";
+	String message = "is on Fire";
+	
+	System.out.println(group + " " + message);
+	
 	
 	System.out.println("Firat Comment");
 	
